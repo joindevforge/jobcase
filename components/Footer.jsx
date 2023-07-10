90212d5ce05de9e620d274ff93339695a61aef37
adityas-opse <85329735+adityas-ops@users.noreply.github.com>
2023-07-10 09:36:24 +0530

    as

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='h-[40px] w-full bg-blue text-white flex items-center justify-center' >
-    <div className='container'>
+    <div className='h-[40px] w-full bg-bluee text-white flex items-center justify-center' >
+    <div className='pl-4'>
     © Jobcase, Inc. 2023 All rights reserved.
     </div>
     </div>
